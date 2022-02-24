9688de38a72841919ff193bfae69c4ba04adaa2d
David Banziziki <dbanzizi@fu-r10-p5.s19.be>
2022-02-24 13:10:52 +0100

    testing cgi stuff

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+    char *env[9] = {"SERVER_PORT=3000",
+                    "SERVER_NAME=localhost",
+                    "SERVER_SOFTWARE=WEBSERV/1.0",
+                    "SERVER_PROTOCOL=HTTP/1.0",
+                    "GATEWAY_INTERFACE=CGI/1.1",
+                    "REQUEST_METHOD=GET",
+                    "SCRIPT_NAME=CGI/ubuntu_cgi_tester",
+                    "PATH_INFO=./",
+                    NULL};
+
+    char *ex = "./ubuntu_cgi_tester";
+    char **arg = NULL;
+    pid_t id = fork();
+    if (id == 0) {
+        execve("./cgi_tester", arg, env);
+        printf("Error: cant find %s\n", ex);
+        exit(EXIT_FAILURE);
+    }
+    waitpid(id, NULL, 0);
+    return 0;
+}
