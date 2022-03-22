c912da404dfae0e512f79a8fcdfe17f56cb718f8
Rodolpho <rodolphocammrosano@gmail.com>
2022-03-22 16:28:05 +0100

    init_response checks for request's content-length against route's body size limit

--- src/request_handlers/CgiPostRH.cpp
+++ src/request_handlers/CgiPostRH.cpp
@@ -13,7 +13,7 @@ ACgiRH(request, table, script_path), bd(*request)
 	else
 	{
 		limit_body = true;
-		max_body_size = request->route->body_size_limit; // TODO: adjust to Mega bytes
+		max_body_size = request->route->body_size_limit * 1024 * 1024;
 	}
 
 	// check if 100-continue is expected
