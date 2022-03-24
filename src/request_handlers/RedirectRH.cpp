2ccb3767204bf1a58eb224ea30bccfed8b82071e
dbanziziki <alexdavid2@live.fr>
2022-03-24 12:28:41 +0100

    default redirect GET && segfault fix

--- src/request_handlers/RedirectRH.cpp
+++ src/request_handlers/RedirectRH.cpp
@@ -1,28 +1,28 @@
 #include "RedirectRH.hpp"
 
 RedirectRH::RedirectRH(HttpRequest *request, FdManager &table)
-    : AReqHandler(request, table) {}
-
-RedirectRH::~RedirectRH() {}
-
-int RedirectRH::respond() {
+    : AReqHandler(request, table) {
     Redirection redirect;
 
-    if (request->route->redirected)
-        redirect = request->route->redirect;
-    else
+    if (request->vserver->redirected)
         redirect = request->vserver->redirect;
+    else
+        redirect = request->route->redirect;
     response.http_version = "HTTP/1.1";
     response.status_code_phrase = long_to_str(redirect.status_code) + ' ' +
                                   reason_phrases[redirect.status_code];
     response.header_fields["location"] = redirect.location;
     assemble_header_str();
-    if (send_header() == 1) {
+}
+
+RedirectRH::~RedirectRH() {}
+
+int RedirectRH::respond() {
+    if (send_header() == 0)
+        return 0;
+    else
         state = s_done;
-        return 1;
-    }
-    state = s_abort;
-    return -1;
+    return 1;
 }
 
 void RedirectRH::abort() { state = s_abort; }
@@ -32,8 +32,10 @@ std::map<int, std::string> RedirectRH::init_map() {
 
     map[301] = "Moved Permanently";
     map[302] = "Found";
-    map[308] = "Permanent Redirect";
+    map[303] = "See Other";
+    map[304] = "Not Modified";
     map[307] = "Temporary Redirect";
+    map[308] = "Permanent Redirect";
     return map;
 }
 
