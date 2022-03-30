658eaf18dbc3953bc4856e30d449c98c3842b206
Rodolpho <rodolphocammrosano@gmail.com>
2022-03-30 16:36:41 +0200

    tester spans o thread per simulated client

--- tests/py_tester2.py
+++ tests/py_tester2.py
@@ -1,8 +1,15 @@
-import requests
+import requests, threading
 
-with requests.Session() as s:
-	for _ in range(100):
-		r = s.get("http://localhost:3000/")
-		r = s.get("http://localhost:3000/form.html")
-		r = s.get("http://localhost:3000/cgi/test.py")
-		r = s.post("http://localhost:3000/cgi/test.py", data={'hello':'you', 'bye':'me'} )
+def client(client_id):
+	print(f"Client {client_id} started")
+	with requests.Session() as s:
+		for _ in range(100):
+			r = s.get("http://localhost:3000/")
+			r = s.get("http://localhost:3000/form.html")
+			r = s.get("http://localhost:3000/cgi/test.py")
+			r = s.post("http://localhost:3000/cgi/test.py", data={'hello':'you', 'bye':'me'} )
+	print(f"Client {client_id} finished")
+
+for client_id in range(20):
+	x = threading.Thread(target=client, args=(client_id,))
+	x.start()
