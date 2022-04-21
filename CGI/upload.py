d44b8c84d3df8526bb545eb1f06e46e03f4f5d16
David Banziziki <alexdavid2@live.fr>
2022-04-21 17:24:31 +0200

    parsing body

--- CGI/upload.py
+++ CGI/upload.py
@@ -14,29 +14,43 @@ def get_file_name(body: str) -> str:
     return file_name
 
 
-def get_file_content(body: str, boundary: str) -> str:
-    res = body.split('\n')
-    # res = [elem.strip() for elem in res if len(elem.strip()) > 0]
-    for i, elem in enumerate(res):
-        if "Content-Type" in elem:
-            content = res[i + 1:-1]
-            eprint(content)
-    return ""
+def parse_body() -> tuple[str, str, str]:
+    boundary = ""
+    file_name = ""
+    for i, line in enumerate(sys.stdin):
+        if i == 0:
+            boundary = line
+        if "Content-Disposition" in line:
+            file_name = get_file_name(line)
+        if line == "\r\n":
+            break
+    # reading file content
+    line = sys.stdin.readline()
+    content = ""
+    while True:
+        if line == '':
+            break
+        next = sys.stdin.readline()
+        if boundary in next:
+            eprint("found end")
+            content += line
+            break
+        content += line
+        line = next
+    return boundary, file_name, content
 
 
 method = os.environ["REQUEST_METHOD"]
 content_type = os.environ["CONTENT_TYPE"]
-boundary = content_type[content_type.find("=") + 1:]
 
-if method == "POST":
-    body = sys.stdin.read()
-else:
-    body = os.environ["QUERY_STRING"]
+boundary, file_name, content = parse_body()
+# eprint(file_name)
+eprint("boundary:", boundary)
+eprint(content)
 
-# content = get_file_content(body, boundary)
-file_name = get_file_name(body)
-# with open(file_name, "w") as f:
-#     f.write(content)
+upload_dir = os.environ["UPLOAD_DIR"]
+# with open("../" + upload_dir + "/" + file_name, "w") as f:
+#     eprint("writing")
 
 response_body = "<!DOCTYPE html>"
 response_body += "<html>"
@@ -45,7 +59,7 @@ response_body += "<title> CGI Script output </title>"
 response_body += "</head>"
 response_body += "<body>"
 response_body += "<p>This is a CGI script to upload files </p>"
-response_body += f'<h4>{file_name} has been created</h4>'
+response_body += f'<h4>{upload_dir} has been created</h4>'
 response_body += "</body>"
 response_body += "</html>"
 print("Content-Type: text/html; charset=UTF-8")
